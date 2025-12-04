af72d721083ab23f1143ba60b364091902603850
Maxilboss93 <massimopisano10@gmail.com>
2025-12-04 16:52:19 +0100

    FIX profilo

--- resources/views/profilo/edit.blade.php
+++ resources/views/profilo/edit.blade.php
@@ -6,12 +6,24 @@
     // $ruolo = $roleName;
     $roleName = $firstRole?->name ?? 'N/A';
     $lastLogin = $user->last_login_at ? $user->last_login_at->format('d/m/Y H:i') : '';
+    function roleLabel($role) {
+        return [
+            'Admin'      => 'Amministrativo ANPAS',
+            'Supervisor' => 'Dipendente ANPAS',
+            'AdminUser'  => 'Amministrativo Associazione',
+            'User'       => 'Utente Associazione',
+        ][$role] ?? $role;
+    }
 
+    $ruoloRaw   = Auth::user()->roles->first()?->name ?? 'N/A';
+    $ruoloLabel = roleLabel($ruoloRaw);
 @endphp
+
 
 <li class="mb-2 d-flex mt-1 justify-content-between"><strong>Nome:</strong><span class="ms-1">{{ $user->firstname }}</span></li>
 <li class="mb-2 d-flex justify-content-between align-items-center"><strong>Cognome:</strong><span class="ms-1">{{ $user->lastname }}</span></li>
 <li class="mb-2 d-flex justify-content-between align-items-center"><strong>Username:</strong><span class="ms-1">{{ $user->username }}</span></li>
+<li class="mb-2 d-flex justify-content-between align-items-center"><strong>Tipologia utente:</strong><span class="ms-1">{{ $ruoloLabel }}</span></li>
 <li class="mb-2 d-flex justify-content-between align-items-center"><strong>Email:</strong><span class="ms-1">{{ $user->email }}</span></li>
 <li class="mb-2 d-flex justify-content-between align-items-center"><strong>Ultimo accesso:</strong> <span class="ms-1">{{ $lastLogin }}</span></li>
 <li class="mb-2 d-flex justify-content-between align-items-center"><strong>Password:</strong>
